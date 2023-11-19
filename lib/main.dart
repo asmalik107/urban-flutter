b7254262645cc9ecb28e0de77ece609607e9768d
asmalik107 <asmalik@outlook.com>
2023-11-19 11:14:12 +0000

    refactor packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
-    title: 'Navigation Basics',
-    home: HomePage(),
-  );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: const Center(
-        child: Text('Home Page'),
-      )
-    );
-  }
-
+  runApp(const App());
 }
